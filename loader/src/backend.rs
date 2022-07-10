bb4d1e1ce6538ef421a08479a1e79ab5e14cadcc
neilcantorne <cantorne.neil@gmail.com>
2022-07-10 19:53:25 +0800

    add backend trait

---
+++ loader/src/backend.rs
@@ -0,0 +1,7 @@
+pub(crate) trait Backend {
+    type Error;
+
+    fn is_installed() -> bool;
+    fn load() -> Result<Self, Self::Error> where Self: Sized;
+    fn release(&self) -> Result<(), Self::Error>;
+}
