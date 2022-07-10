e4ff72b0610f05acff08095b5887b527fcf27b5a
neilcantorne <cantorne.neil@gmail.com>
2022-07-10 21:49:49 +0800

    add templates for lib loader implementation

---
+++ loader/src/library/internal/lib_linux.rs
@@ -0,0 +1,14 @@
+pub(crate) struct Library {
+
+}
+
+impl Library {
+    pub unsafe fn load(name: &str) -> Result<Self, crate::library::LoadError> {
+        todo!();
+    }
+
+    pub unsafe fn get_fn<T>(&self, symbol: &str) -> Option<T>{
+        todo!();
+    }
+}
+
