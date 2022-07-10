73994060f4ad27a27916551c310212b9f33b79ad
neilcantorne <cantorne.neil@gmail.com>
2022-07-11 00:26:33 +0800

    add ffi for libdl.so

--- loader/src/library/internal/lib_linux.rs
+++ loader/src/library/internal/lib_linux.rs
@@ -1,3 +1,5 @@
+use std::{os::raw::c_char, ffi::c_void};
+
 pub(crate) struct Library {
 
 }
@@ -12,3 +14,14 @@ impl Library {
     }
 }
 
+// FFI with Linux library loader
+type lib_handle = *const c_void;
+const RTLD_LAZY: i32 = 0x00001;
+
+#[link(name="dl", kind="dylib")]
+extern "C" {
+    fn dlopen(filename: *const c_char, flags: i32) -> lib_handle;
+    fn dlerror() -> *const c_char;
+    fn dlsym(handle: lib_handle, symbol: *const c_char) -> lib_handle;
+    fn dlclose(handle: lib_handle) -> i32;
+}
