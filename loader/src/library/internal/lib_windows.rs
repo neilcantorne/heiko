a488c6bec31f21ccd4c6d0a8ab2291e0263948f0
neilcantorne <cantorne.neil@gmail.com>
2022-07-11 07:52:23 +0800

    add ffi for kernel32.dll

--- loader/src/library/internal/lib_windows.rs
+++ loader/src/library/internal/lib_windows.rs
@@ -1,9 +1,14 @@
-pub(crate) struct Library {
+#![allow(non_camel_case_types)]
+#![allow(dead_code)]
 
+use std::{os::raw::c_char, ffi::{c_void, CString, CStr}};
+
+pub(crate) struct Library {
+    handle : lib_handle
 }
 
 impl Library {
-    pub unsafe fn load(name: &str) -> Result<Self, crate::library::LibraryError> {
+    pub unsafe fn load(name: &str) -> Result<Self, crate::library::LoadError> {
         todo!();
     }
 
@@ -12,3 +17,13 @@ impl Library {
     }
 }
 
+// FFI with Windows library loader
+type lib_handle = *const c_void;
+
+#[link(name="kernel32", kind="dylib")]
+extern "C" {
+    fn LoadLibraryA(filename: *const c_char) -> lib_handle;
+    fn GetLastError() -> u32;
+    fn GetProcAddress(handle: lib_handle, symbol: *const c_char) -> lib_handle;
+    fn FreeLibrary(handle: lib_handle) -> bool;
+}
