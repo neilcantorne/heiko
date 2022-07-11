93bca6029f3a21e8918038057e3b7a3b4ea93ff3
neilcantorne <cantorne.neil@gmail.com>
2022-07-11 18:58:56 +0800

    impl is_installed

---
+++ loader/src/cl/cl_backend.rs
@@ -0,0 +1,35 @@
+use crate::library::Library;
+use std::path::Path;
+
+pub(crate) struct CLBackend {
+    library: Library
+}
+
+#[cfg(target_os = "linux")]
+const LIBNAME : &str = "libOpenCL.so";
+
+#[cfg(target_os = "windows")]
+const LIBNAME : &str = "OpenCL.dll";
+
+#[cfg(target_os = "macos")]
+const LIBNAME : &str = "libOpenCL.dylib";
+
+impl crate::backend::Backend for CLBackend {
+    type Error = Error;
+
+    fn is_installed() -> bool {
+        Library::lib_check(&[LIBNAME])[0]
+    }
+
+    fn load() -> Result<Self, Self::Error> where Self: Sized {
+        todo!()
+    }
+
+    fn release(&self) -> Result<(), Self::Error> {
+        todo!()
+    }
+}
+
+pub(crate) struct Error {
+    
+}
