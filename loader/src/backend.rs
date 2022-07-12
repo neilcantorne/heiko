c2db63d0f82026d336e29a1ab93d9bbad6aec610
neilcantorne <cantorne.neil@gmail.com>
2022-07-12 08:34:33 +0800

    replace with LoadError

--- loader/src/backend.rs
+++ loader/src/backend.rs
@@ -1,6 +1,4 @@
 pub(crate) trait Backend {
-    type Error;
-
     fn is_installed() -> bool;
-    fn load() -> Result<Self, Self::Error> where Self: Sized;
+    fn load() -> Result<Self, crate::library::LoadError> where Self: Sized;
 }
