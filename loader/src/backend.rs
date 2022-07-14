52a02ac510c1f5a41e297503f68025b2a72eb34b
neilcantorne <cantorne.neil@gmail.com>
2022-07-14 13:48:31 +0800

    replace with handle

--- loader/src/backend.rs
+++ loader/src/backend.rs
@@ -1,4 +1,6 @@
 pub(crate) trait Backend {
+    type Handle;
     fn is_installed() -> bool;
-    unsafe fn load() -> Result<Self, crate::library::LoadError> where Self: Sized;
+    unsafe fn load() -> Result<Self::Handle, crate::library::LoadError> 
+        where Self::Handle: Sized + Drop + Sync;
 }
